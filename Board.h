7a95708b713655769609f1685b92b37a9985c926
BrentTB <btbutkow@gmail.com>
2023-11-24 16:49:47 +0200

    added comments

--- Board.h
+++ Board.h
@@ -1,20 +1,19 @@
 #ifndef BOARD
 #define BOARD
 
-// #include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 using GameBoard = vector<vector<vector<char>>>;
 #define endd cout << endl
-#define spc  "        "
+#define spc "        "
 const char defaultChar = ' ';
 const char winChar = 'W';
 
 /**
  * @brief Exception class for if a chosen spot already has a piece
- * 
+ *
  */
 class UsedSpot
 {
@@ -22,40 +21,68 @@ class UsedSpot
 
 /**
  * @brief Exception class for if a chosen spot is invalid / off the board
- * 
+ *
  */
 class InvalidSpot
 {
 };
 
+/**
+ * @brief Class that represents the board of a 3d 4x4 tic tac toe game
+ *
+ */
 class Board
 {
 public:
     /**
      * @brief Construct a new Board object
-     * 
+     *
      */
     Board();
 
     /**
-     * @brief display the current board
-     * 
+     * @brief Display the current board in the terminal
+     *
      */
     void printBoard();
 
-
+    /**
+     * @brief places a specified 'winning' piece at the specified square, regardless of if that square is occupied or not
+     *
+     * @param board indicates the board
+     * @param row indicates the row
+     * @param col indicates the column
+     */
     void placeWinPiece(int board, int row, int col);
 
+    /**
+     * @brief places the given piece at the specified square, if the location given is valid
+     *
+     * @param piece the char of the piece being placed
+     * @param board indicates the board
+     * @param row indicates the row
+     * @param col indicates the column
+     */
     void placePiece(char piece, int board, int row, int col);
 
+    /**
+     * @brief Get the piece at the location specified
+     *
+     * @param board indicates the board
+     * @param row indicates the row
+     * @param col indicates the column
+     * @return char the piece at the given location
+     */
     char getPiece(int board, int row, int col);
 
-    void printBoardVertical();
-
+    /**
+     * @brief Destroy the Board object
+     *
+     */
     ~Board();
 
 private:
-    GameBoard gameBoard_;
+    GameBoard gameBoard_; // stores the 4x4x4 game board
 };
 
 #endif
--- Game.h
+++ Game.h
@@ -1,7 +1,6 @@
 #ifndef GAME
 #define GAME
 
-// #include <bits/stdc++.h>
 #include <iostream>
 #include <stdlib.h>
 #include "Board.h"
@@ -9,11 +8,24 @@
 
 using namespace std;
 
+/**
+ * @brief Class used to run a game of 3d 4x4 tic tac toe
+ * 
+ */
 class Game
 {
 public:
+    /**
+     * @brief Construct a new Game object
+     *
+     */
     Game();
 
+    /**
+     * @brief Starts a game of 3d 4x4 tic tac toe, either with 2 players or a bot
+     *
+     * @param twoPlayer false if the player is playing a bot
+     */
     void startGame(const bool twoPlayer);
 
     /**
@@ -23,19 +35,55 @@ public:
     ~Game();
 
 private:
+    /**
+     * @brief Runs the game and calls all other functions
+     *
+     */
     void gameLoop();
+
+    /**
+     * @brief Display the current state of the board
+     *
+     */
     void showBoard();
+
+    /**
+     * @brief Get the Input object gets and sanitises the user's input / gets the bot's input
+     *
+     */
     void getInput();
+
+    /**
+     * @brief Find if the game has been won. If it has, the winning pieces are changed to another symbol
+     *
+     * @param piece the char representing one of the players/bot
+     * @return true if the given piece has won
+     * @return false otherwise
+     */
     bool checkWin(const char piece);
+
+    /**
+     * @brief Takes in four pieces and returns if all of them belong to the same side
+     *
+     * @param piece the player/bot's char to be checked
+     * @param moves a vector of the grid squares to be checked
+     * @return true if all of the board places indicated by moves are fulled by the piece's char
+     * @return false otherwise
+     */
     bool checkPiece(const char piece, vector<tuple<int, int, int>> &moves);
+
+    /**
+     * @brief Displays the board and a winning message at the end of the game
+     *
+     */
     void displayWin();
 
-    Board gameBoard_;
-    Bot opponent_;
-    bool xTurn_;
-    bool useBot_;
-    char win_;
-    tuple<int, int, int> input_;
+    Board gameBoard_;            // stores the moves that have been played
+    Bot opponent_;               // stores the bot to play against
+    bool xTurn_;                 // if it is X's turn or O's turn
+    bool useBot_;                // if the opponent is a bot / player
+    char win_;                   // stores which player/bot has one
+    tuple<int, int, int> input_; // stores the user / bots input
 };
 
 #endif
