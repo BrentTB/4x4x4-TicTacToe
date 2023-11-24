218d26ca28455921c1c7dda4bc1ad2b5646d9463
BrentTB <btbutkow@gmail.com>
2023-11-24 10:34:51 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define uint unsigned int
+#define ull unsigned long long
+#define endl "\n"
+#define elif else if
+#define fo(i, a, b) for (int i = a; i < (int)b; i++)
+#define rfo(i, a, b) for (int i = a - 1; i >= b; i--)
+#define v(i) vector<i>
+#define vll vector<long long>
+#define vint vector<int>
+#define vstr vector<string>
+#define pairll pair<long long, long long>
+#define vpairll vector<pair<long long, long long>>
+#define print(x) cout << x << "\n"
+
+#define printall(x)         \
+    for (auto zz : x)       \
+    {                       \
+        cout << zz << "\n"; \
+    }
+
+#define printallpair(x)            \
+    for (auto zz : x)              \
+    {                              \
+        cout << zz.first << " ";   \
+        cout << zz.second << "\n"; \
+    }
+
+#define pushall(x, n)                   \
+    x.resize(n);                        \
+    for (int zz = 0; zz < (int)n; zz++) \
+    {                                   \
+        cin >> x[zz];                   \
+    }
+
+#define pushallpair(x, n)                   \
+    x.resize(n);                            \
+    for (int zz = 0; zz < (int)n; zz++)     \
+    {                                       \
+        cin >> x[zz].first >> x[zz].second; \
+    }
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    // int test[20][5] = {[0 ... 9][0 ... 4] = 5, [10 ... 19][0 ... 2] = 3, [10 ... 19][3 ... 4] = 100};
+
+    // fo(i, 0, 20)
+    // {
+    //     fo(j, 0, 5)
+    //     {
+    //         cout << test[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+}
